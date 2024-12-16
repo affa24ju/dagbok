d687ce0fdcdc1c800644328aa92bc37ba94385d2
Fatema Afroza <fafroza2017@gmail.com>
2024-12-16 19:05:56 +0100

    Skapat class

---
+++ src/main/java/com/dagbok/dagbok/Dairy.java
@@ -0,0 +1,48 @@
+package com.dagbok.dagbok;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Dairy{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String title;
+    private Date date;
+    private String text;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    //Getters & setters
+    
+
+    
+}
