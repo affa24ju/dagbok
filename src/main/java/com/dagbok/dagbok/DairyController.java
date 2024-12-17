b3f87ec406f217af5db708fc70f286bc0b6f2aff
Fatema Afroza <fafroza2017@gmail.com>
2024-12-17 09:25:22 +0100

    Lagt till form utan funktionalitet för att lägga till nytt inlägg

--- src/main/java/com/dagbok/dagbok/DairyController.java
+++ src/main/java/com/dagbok/dagbok/DairyController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -18,7 +19,7 @@ public class DairyController {
         return "index";
     }
 
-    @GetMapping("/new-post")
+    @PostMapping("/new-post")
     public String addNew() {
 
         //Dairy dairy = new Dairy();
